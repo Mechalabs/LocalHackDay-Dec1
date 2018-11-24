f326cb9ec01f526f624b608c81481dbc20a59dad
supermule <45317559+supermule@users.noreply.github.com>
2018-11-24 14:24:07 -0500

    Update minigame 2

--- Minigame2.py
+++ Minigame2.py
@@ -15,6 +15,10 @@ icon =  pygame.image.load('heart.png')
 icony = 300
 x = 750
 s = randint(0,500)
+font = pygame.font.SysFont("Comic Sans",72)
+inPlay = True
+win = False
+count = 0
 
 def update(x):
     pygame.event.clear()
@@ -29,8 +33,20 @@ def update(x):
     block2.fill(Color("WHITE"), rectangle2)
     pygame.display.update()
     time.sleep(0.005)
+    
+def gameover():
+    gamewindow.fill(BLACK)
+    graphics = font.render("Game Over",1,WHITE)
+    gamewindow.blit(graphics,(270,250))
+    pygame.display.update()
 
-while True:
+def wingame():
+    gamewindow.fill(BLACK)
+    graphics = font.render("Good job you win!",1,WHITE)
+    gamewindow.blit(graphics, (200,250))
+    pygame.display.update()
+    
+while inPlay:
     keys=pygame.key.get_pressed()
     if keys[K_UP]:
         icony-=3
@@ -39,8 +55,20 @@ while True:
     if(x == 0):
         s = randint(0,500)
         x = 750
+        count = count+1
     else:
         s == s
         x = x-2
+    if(x <= 80):
+        if(icony <= s or icony >= s+20):
+            inPlay = False
+    if(count == 10):
+        win = True
+        inPlay = False
+
     update(x)
 
+if(win == False):
+    gameover()
+else:
+    wingame()
