f145b843efcb127f674f2cf36c0fc64fa8402bb4
Nightsavage <booksareawesomeness13@gmail.com>
2018-11-26 11:58:39 -0500

    Update Starting Page.py

--- Starting Page.py
+++ Starting Page.py
@@ -14,8 +14,8 @@ pygame.font.init()
 pygame.mixer.init()
 font = pygame.font.SysFont("Comic Sans MS", 36)
 
-
-Gaster = pygame.image.load("C:\Users\user\Desktop\Meccha Labs\Undertale Game Thing\Gaster_Sprite2.png")
+Narwhal = pygame.image.load("C:\Users\user\Desktop\Meccha Labs\Undertale Game Thing\Evil Narwhal.png")
+##Gaster = pygame.image.load("C:\Users\user\Desktop\Meccha Labs\Undertale Game Thing\Gaster_Sprite2.png")
 
 ## SHOULD ADD GLITCHES ON PURPOSE
 
@@ -26,7 +26,7 @@ def battlepage():
     gameWindow.fill(BLACK)
     pygame.draw.rect(gameWindow, WHITE, (50, 600, 300, 100), 1)
     pygame.draw.rect(gameWindow, WHITE, (450, 600, 300, 100), 1)
-    gameWindow.blit(Gaster, (300, 50))
+    gameWindow.blit(Narwhal, (155, 20))
     graphics1 = font.render("Fight", 1, WHITE)
     graphics2 = font.render("Mercy", 1, WHITE)
     gameWindow.blit(graphics1, (150, 620))
